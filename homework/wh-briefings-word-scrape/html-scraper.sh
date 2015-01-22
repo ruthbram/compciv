e63c4964bfab2a51b56ed91a2d9b3c32368b7a4b
Ruth Clausen Bram <rcbram@corn30.stanford.edu>
2015-01-21 18:40:35 -0800

    This is the final

--- homework/wh-briefings-word-scrape/html-scraper.sh
+++ homework/wh-briefings-word-scrape/html-scraper.sh
@@ -1,5 +1,5 @@
-cat data-hold/* | pup "p text{}" > data-hold/textonly.txt
-cat data-hold/* | pup "div.legacy-para text{}" >  data-hold/othertext.txt
+cat data-hold/* | pup "p text{}" > textonly.txt
+cat data-hold/* | pup "div.legacy-para text{}" >  othertext.txt
 
 cat textonly.txt othertext.txt| grep -o -E '[[:alpha:]]{7,}' | sort | uniq -c | sort -r | head -n 10
 
