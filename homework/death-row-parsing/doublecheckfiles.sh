94ea69145bc2d264e5343d5be1e0501f02d934a9
Ruth Clausen Bram <rcbram@corn26.stanford.edu>
2015-01-28 00:40:34 -0800

    dr hw

---
+++ homework/death-row-parsing/doublecheckfiles.sh
@@ -0,0 +1,16 @@
+# downloads all files
+bash helper.sh
+
+# shows "data-hold" and "helper.sh"
+ls
+
+# change directory 
+cd data-hold
+
+# shows my files: cadrcurr.html  flexlistcurr.html  txdrcurr.html fldrcurr.html  flexlistpast.html  txdrpast.html
+ls
+
+
+
+
+
