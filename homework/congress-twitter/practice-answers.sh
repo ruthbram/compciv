f89eab0c2db429a5bcbbe851ca486bb475e5df7d
Ruth Clausen Bram <rcbram@corn24.stanford.edu>
2015-01-27 20:45:57 -0800

    jq practice answers

---
+++ homework/congress-twitter/practice-answers.sh
@@ -0,0 +1,364 @@
+1.
+"Brown"
+2.
+"D000604"
+3.
+"2001-01-03"
+4.
+"Robert"
+"Casey"
+"1960-04-13"
+5.
+1984-07-02
+1983-03-30
+1981-05-28
+1981-04-12
+1980-11-16
+1980-10-04
+1980-06-16
+1980-04-18
+1980-03-01
+1980-01-30
+6.
+    162 null
+      8 null
+      6 null
+      6 null
+      5 null
+      5 null
+      5 null
+      5 null
+      4 null
+      4 null
+      4 null
+      4 null
+      4 null
+      3 null
+      3 null
+      3 null
+      3 null
+      3 null
+      3 null
+      3 null
+      3 null
+      3 null
+      3 null
+      3 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 null
+      2 Roman Catholic
+      2 Roman Catholic
+      2 Roman Catholic
+      2 Roman Catholic
+      2 Roman Catholic
+      2 Roman Catholic
+      2 Methodist
+      2 Catholic
+      2 Baptist
+      2 Baptist
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 null
+      1 Unknown
+      1 Unknown
+      1 Unknown
+      1 Unknown
+      1 Unknown
+      1 United Methodist
+      1 Southern Baptist
+      1 Southern Baptist
+      1 Seventh Day Adventist
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Roman Catholic
+      1 Protestant
+      1 Protestant
+      1 Protestant
+      1 Protestant
+      1 Protestant
+      1 Protestant
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Presbyterian
+      1 Nazarene
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Methodist
+      1 Lutheran
+      1 Lutheran
+      1 Lutheran
+      1 Lutheran
+      1 Lutheran
+      1 Lutheran
+      1 Latter Day Saints
+      1 Latter Day Saints
+      1 Latter Day Saints
+      1 Latter Day Saints
+      1 Latter Day Saints
+      1 Latter Day Saints
+      1 Latter Day Saints
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Jewish
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopalian
+      1 Episcopal
+      1 Church of Christ
+      1 Christian
+      1 Christian
+      1 Christian
+      1 Christian
+      1 Christian
+      1 Christian Scientist
+      1 Christian Scientist
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Catholic
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 Baptist
+      1 African Methodist Episcopal
+      1 African Methodist Episcopal
+      1 African Methodist Episcopal
+7.
+64
+8.
+2015
+9.
+301
+10.
+3
+11.
+feed://olson.house.gov/common/rss/?rss=82
+http://aderholt.house.gov/common/rss//index.cfm?rss=20
+http://adriansmith.house.gov/rss.xml
+http://algreen.house.gov/rss.xml
+http://amash.house.gov/rss.xml
+http://amodei.house.gov/common/rss//?rss=49
+http://austinscott.house.gov/index.php?format=feed&amp;type=rss
+http://ayotte.senate.gov/rss/?p=news
+http://barletta.house.gov/common/rss//index.cfm?rss=25
+http://barr.house.gov/rss.xml
+12.
+490
+13.
+439
+14.
+"Y000064","RepToddYoung"
+"Y000063","RepKevinYoder"
+"Y000062","RepJohnYarmuth"
+"Y000033","RepDonYoung"
+"W000809","Rep_SteveWomack"
+"W000808","RepWilson"
+"W000806","RepWebster"
+"W000805","MarkWarner"
+"W000804","RobWittman"
+"W000802","SenWhitehouse"
+15.
+"S001177",
+"M001144",
+"M000689",
+"D000604",
+"C001049",
+"C001037",
+"A000368",
+"B001245",
+"G000546",
+"W000810",
+16.
+463
+17.
+1938080
+18.
+"Aaron Schock","aaronschock",25383,true,"Thu Mar 12 14:04:15 +0000 2009"
+"Ander Crenshaw","AnderCrenshaw",8229,true,"Fri Feb 06 01:48:11 +0000 2009"
+"Rep. Austin Scott","AustinScottGA08",7326,true,"Thu Jan 06 16:01:46 +0000 2011"
+"RepBThompson","BennieGThompson",4401,true,"Wed Oct 14 20:47:03 +0000 2009"
+"Betty McCollum","BettyMcCollum04",5784,true,"Tue Mar 06 20:52:11 +0000 2012"
+"Bill Pascrell, Jr.","BillPascrell",4201,true,"Tue Sep 15 17:54:32 +0000 2009"
+"Blumenauer Media","BlumenauerMedia",2732,true,"Tue Apr 27 21:48:06 +0000 2010"
+"U.S. Rep. Bob Latta","boblatta",14499,true,"Fri Jul 11 18:33:24 +0000 2008"
+"Brad Sherman","BradSherman",7093,true,"Fri Apr 10 13:38:03 +0000 2009"
+"Dutch Ruppersberger","Call_Me_Dutch",4983,true,"Thu May 26 14:39:08 +0000 2011"
+19.
+"aaronschock",2502,25383,"Thu Mar 12 14:04:15 +0000 2009"
+"AnderCrenshaw",827,8229,"Fri Feb 06 01:48:11 +0000 2009"
+"AustinScottGA08",1388,7326,"Thu Jan 06 16:01:46 +0000 2011"
+"BennieGThompson",417,4401,"Wed Oct 14 20:47:03 +0000 2009"
+"BettyMcCollum04",2449,5784,"Tue Mar 06 20:52:11 +0000 2012"
+"BillPascrell",803,4201,"Tue Sep 15 17:54:32 +0000 2009"
+"BlumenauerMedia",356,2732,"Tue Apr 27 21:48:06 +0000 2010"
+"boblatta",4865,14499,"Fri Jul 11 18:33:24 +0000 2008"
+"BradSherman",516,7093,"Fri Apr 10 13:38:03 +0000 2009"
+"Call_Me_Dutch",1584,4983,"Thu May 26 14:39:08 +0000 2011"
