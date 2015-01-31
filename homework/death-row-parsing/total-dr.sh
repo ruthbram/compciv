b53d6482d934ef530387f9aad87ccc7132c7e2ec
Ruth Clausen Bram <rcbram@corn09.stanford.edu>
2015-01-30 21:03:29 -0800

    death row hw

---
+++ homework/death-row-parsing/total-dr.sh
@@ -0,0 +1,3 @@
+bash tx-dr.sh | sed s/White/TX,White/ | sed s/Hispanic/TX,Hispanic/ | sed s/Black/TX,Black/ | sed s/Other/TX,Other/
+bash fl-dr.sh | sed s/WM/FL,White/ | sed s/WF/FL,White/ | sed s/HM/FL,Hispanic/ | sed s/HF/FL,Hispanic/ | sed s/BM/FL,Black/ | sed s/BF/FL,Black/ | sed s/OM/FL,Other/ | sed s/OF/FL,Other/
+bash ca-dr.sh | sed s/WHI/CA,White/ | sed s/HIS/CA,Hispanic/ | sed s/BLA/CA,Black/ | sed s/OTH/CA,Other/
