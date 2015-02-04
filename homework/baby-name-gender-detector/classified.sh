e6e5f5ee0ec696ad9101e55b17a54c2e1bbb6f07
Ruth Bram <rcbram@stanford.edu>
2015-02-03 23:59:52 -0800

    Update classified.sh

--- homework/baby-name-gender-detector/classified.sh
+++ homework/baby-name-gender-detector/classified.sh
@@ -2,30 +2,31 @@ datafile='data-hold/namesample.txt'
 
 if [[ $# -eq 0 ]]
 then
-	echo 'Type in at least one name.'
+echo 'Type in at least one name.'
 else
 	for name in "$@"
 	do
-		name_matches=$(cat $datafile | grep "$name")
-		m_count=0
-		f_count=0
+	name_matches=$(cat $datafile | grep "$name")
+	
+	m_count=0
+	f_count=0
 
-		for row in $name_matches
-		do
-			babies=$(echo $row | cut -d ',' -f '3')
-		if [[ $row =~ ',M,' ]]
-		then
-			m_count=$((m_count + babies))
-		else
-			f_count=$((f_count + babies))
+	for row in $name_matches
+	do
+		babies=$(echo $row | cut -d ',' -f '3')
+	if [[ $row =~ ',M,' ]]
+	then
+		m_count=$((m_count + babies))
+	else
+		f_count=$((f_count + babies))
 		fi
 		done
 
-		total_babies=$((m_count + f_count))
+	total_babies=$((m_count + f_count))
 
-		if [[ $total_babies -eq 0 ]]
-		then
-			echo "There's 0 babies with the name: $name"
+	if [[ $total_babies -eq 0 ]]
+	then
+	echo "There's 0 babies with the name: $name"
 		else
 			pct_female=$((100 * f_count / total_babies))
 			if [[ $pct_female -ge 50 ]]
