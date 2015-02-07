bd9cdf66ddbbed8d8d7d281d0fcb2a6725a0a378
Ruth Clausen Bram <rcbram@corn36.stanford.edu>
2015-02-07 10:27:12 -0800

    more edits

--- homework/usajobsgov/scraper.sh
+++ homework/usajobsgov/scraper.sh
@@ -1,10 +1,9 @@
 dir=$(date +%Y-%m-%d_%H00)
 mkdir -p data-hold/scrapes/$dir
 
-cat data-hold | grep -oE '<JobFamily>[0-9]{4}' | grep -oE '[0-9]+' | sort | uniq 
+jobcodes=$(cat data-hold | grep -oE '<JobFamily>[0-9]{4}' | grep -oE '[0-9]$)
 
 url="https://data.usajobs.gov/api/jobs?page=1&numberofjobs=250
-
 
 for jobcode in $jobcodes  
         do
@@ -13,14 +12,11 @@ for jobcode in $jobcodes
         curl -s "$url=$jobcode" -o $fname
                 echo curl -s "$url=$jobcode" -o $fname
         total_pages=$(cat $fname | jq -r '.Pages')
-
 for pnum in $(seq 2 $total_pages)
         do
         url="https://data.usajobs.gov/api/jobs?page=pnum&NumberOfJobs=250&series" 
         fname="$dir/jobcode-$pnum.json"
-        curl -s "$url=$jobcode" -o $fname       
+        curl -s "$url=$jobcode" -o $fname
         echo curl -s "$url=$jobcode" -o $fname
         done
-done
-
 done
