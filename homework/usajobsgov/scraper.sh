5626ba6cfeb4d584c22646bb387c26c6a7872f4f
Ruth Clausen Bram <rcbram@corn36.stanford.edu>
2015-02-07 11:38:30 -0800

    scraper finished

--- homework/usajobsgov/scraper.sh
+++ homework/usajobsgov/scraper.sh
@@ -1,17 +1,18 @@
-dir=$(date +%Y-%m-%d)
-mkdir -p data-hold/scrapes/$dir
+thedate=$(date +%Y-%m-%d)
+thedir="data-hold/scrapes/$thedate"
+mkdir -p $thedir
 
 jobcodes=$(cat data-hold/OccupationalSeries.xml | grep -oE '<JobFamily>[0-9]{4}' | grep -oE '[0-9]{4}' | sort | uniq)
 
-url="https://data.usajobs.gov/api/jobs?page=1&numberofjobs=250"
+url="https://data.usajobs.gov/api/jobs?page=1&numberofjobs=250&series"
 
 
 for jobcode in $jobcodes
         do
-        #url="https://data.usajobs.gov/api/jobs?page=1&NumberOfJobs=250&series" 
-        #fname="$dir/jobcode-$page_number.json"
-        #curl -s "$url=$jobcode" -o $fname
-        #        echo curl -s "$url=$jobcode" -o $fname
-        #total_pages=$(cat $fname | jq -r '.Pages')
-echo $jobcodes
+        url="https://data.usajobs.gov/api/jobs?page=1&NumberOfJobs=250&series"
+        fname="$thedir/$jobcode$page_number.json"
+        curl -s "$url=$jobcode" -o $fname
+                echo curl -s "$url=$jobcode" -o $fname
+        total_pages=$(cat $fname | jq -r '.Pages')
+
 done
