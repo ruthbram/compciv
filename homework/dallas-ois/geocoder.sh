cfe53f20c7377fdccdaadd665bce535fc61928c9
Ruth Clausen Bram <rcbram@corn02.stanford.edu>
2015-03-09 01:48:06 -0700

    trying to figure out the geocoder

--- homework/dallas-ois/geocoder.sh
+++ homework/dallas-ois/geocoder.sh
@@ -16,18 +16,24 @@ mkdir -p 'data-hold/geocodes'
 
 #Remove non-alphanum characters
 some_location="2502 Grand Avenue"
-some_slug=$(echo $some_location | sed s/[^A-z0-9]//g) # 2502GrandAvenue
+some_slug=$(echo $some_location | sed s/[^A-z0-9]//g)
 some_filename="data-hold/geocodes/$some_slug.json"
 
 #Caching strategy
 #If file exists and is non-zero, then we don't need to download it
-if [[ -s "$some_filename" ]]
+if [[ -s "data-hold/geocodes/$some_filename" ]]
 then
 echo "Already downloaded $some_filename"
 else
-echo "Going to geocode the file"
+echo "File is being geocoded"
 curl "https://maps.googleapis.com/maps/api/geocode/json?address=Dallas+TX"
 fi
+
+#Retrieve coordinates
+loc=$(echo $some_location | jq '.results[0] | .formatted_address' | tr -d ',')
+lat=$(cat $some_filename | jq '.results[0] | .geometry .location .lat')
+lng=$(cat $some_filename | jq '.results[0] | .geometry .location .lng')
+
 
 #Output final results & produce PSV text using the printf tool
 printf '%s|%s|%s|\n' "$loc" "$lat" "$lng" >> tables/geocodes.psv
