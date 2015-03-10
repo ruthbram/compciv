9e12dd48e653c94be8221c45693ea516060ca735
Ruth Clausen Bram <rcbram@corn39.stanford.edu>
2015-03-09 17:08:14 -0700

    finished geocoder

--- homework/dallas-ois/geocoder.sh
+++ homework/dallas-ois/geocoder.sh
@@ -1,40 +1,28 @@
-#Parse the information (Hint section) & output to CSV file
+storedcodes="data-hold/geocodes/"
+mkdir -p $storedcodes
+
+baseurl="https://maps.googleapis.com/maps/api/geocode/json?address="
+
+#Parse the information & output to CSV file (Hint section)
 cat data-hold/*.html | pup 'table table tr json{}' | jq  --raw-output '.[] .children | [
     .[0] .children[0] .href,
     .[0] .children[0] .text,
-    .[1] .text ,
+    .[1] .text,
     .[2] .text,
     .[3] .text,
     .[4] .text,
     .[5] .text,
     .[6] .text,
-    .[7] .text
-  ] | @csv' > geocodes_ois.csv
+    .[7] .text]
+   | @csv' |  csvfix order  -f 4 | tr -d '"' | grep -v 'Location' | while read -r address; do
 
-#Create data-hold folder to hold geocodes (a caching strategy)
-mkdir -p 'data-hold/geocodes'
-
-#Remove non-alphanum characters
-some_location="2502 Grand Avenue"
-some_slug=$(echo $some_location | sed s/[^A-z0-9]//g)
-some_filename="data-hold/geocodes/$some_slug.json"
-
-#Caching strategy
-#If file exists and is non-zero, then we don't need to download it
-if [[ -s "data-hold/geocodes/$some_filename" ]]
-then
-echo "Already downloaded $some_filename"
-else
-echo "File is being geocoded"
-curl "https://maps.googleapis.com/maps/api/geocode/json?address=Dallas+TX"
-fi
-
-#Retrieve coordinates
-loc=$(echo $some_location | jq '.results[0] | .formatted_address' | tr -d ',')
-lat=$(cat $some_filename | jq '.results[0] | .geometry .location .lat')
-lng=$(cat $some_filename | jq '.results[0] | .geometry .location .lng')
-
-
-#Output final results & produce PSV text using the printf tool
-printf '%s|%s|%s|\n' "$loc" "$lat" "$lng" >> tables/geocodes.psv
+    url=$(echo "https://maps.googleapis.com/maps/api/geocode/json?address=$address,Dallas,TX" | tr ' ' '+')
+    echo $url
+#download all urls with addresses (use sleep command)
+  fname="$(echo $address | tr ' ' '+').json"
+  curl -s "$url" > data-hold/geocodes/$fname
+  echo $fname
+   # curl -s "$url" >> data-hold/geocodes/geocodes.json
+sleep 1
 done
+
