bc51b8906926b57eb23f443318575f5324a4a40a
Ruth Clausen Bram <rcbram@corn32.stanford.edu>
2015-03-11 03:17:38 -0700

    rough tablemaker2/pagemaker

--- homework/dallas-ois/geocoder.sh
+++ homework/dallas-ois/geocoder.sh
@@ -14,15 +14,19 @@ cat data-hold/*.html | pup 'table table tr json{}' | jq  --raw-output '.[] .chil
     .[5] .text,
     .[6] .text,
     .[7] .text]
-   | @csv' |  csvfix order  -f 4 | tr -d '"' | grep -v 'Location' | while read -r address; do
+   | @csv' |  csvfix order  -f 4 | tr -d '"' | grep -v 'Location'
+while read -r address
+do
 
-    url=$(echo "https://maps.googleapis.com/maps/api/geocode/json?address=$address,Dallas,TX" | tr ' ' '+')
-    echo $url
+ url=$(echo "https://maps.googleapis.com/maps/api/geocode/json?address=$address,Dallas,TX" | tr ' ' '+')
+ echo $url
+
 #download all urls with addresses (use sleep command)
-  fname="$(echo $address | tr ' ' '+').json"
-  curl -s "$url" > data-hold/geocodes/$fname
-  echo $fname
-   # curl -s "$url" >> data-hold/geocodes/geocodes.json
+fname="$(echo $address | tr ' ' '+').json"
+curl -s "$url" > data-hold/geocodes/$fname
+Sleep 2
+echo $fname
+# curl -s "$url" >> data-hold/geocodes/geocodes.json
 
 done
 
--- homework/dallas-ois/tablemaker2.sh
+++ homework/dallas-ois/tablemaker2.sh
@@ -1,6 +1,40 @@
-#Header for Incidents
+#HEADER for INCIDENTS
 echo 'case_number|date|location|suspect_status|suspect_weapon|suspects|officers|grand_jury|latitude|longitude|narrative'
 # > tables/incidents.psv
+cat data-hold/*.html | pup 'table table tr json{}' | jq  --raw-output '.[] .children | [
+    .[0] .children[0] .href,
+    .[0] .children[0] .text,
+    .[1] .text,
+    .[2] .text,
+    .[3] .text,
+    .[4] .text,
+    .[5] .text,
+    .[6] .text,
+    .[7] .text]
+   | @csv' |  csvfix order -osep '|'  -f 1,2,3,4,5,6,7,8,9,10,11 
+while read -r incident
+do
+
+cat geocodes.psv
+echo "$incident"
+
+blah="geocodes.psv"
+latitude=$(cat geocodes.psv/$blah | csvfix sort -f 2 | grep 'location' | cut -d "|" -f 2`)
+longitude=$(cat tables/$blah | csvfix sort -f 3 | grep 'location' | cut -d "|" -f 3)
+
+blob="data-hold/pdfs/OIS_year_$.txt"
+narrative=$(cat $blob | grep 'narrative')
+
+
+# Print all final fields for Incidents
+echo "$case_number|$date|$address|$suspect_status|$suspect_weapon|$suspects|$officers|$grand_jury|$latitude|$longitude|$narrative"
+# >> tables/incidents.psv
+done
+
+
+#HEADER for OFFICERS
+echo 'case_number|date|suspect_killed|suspect_weapon|last_name|first_name|race|gender'
+#> tables/officers.psv
 
 cat data-hold/*.html | pup 'table table tr json{}' | jq  --raw-output '.[] .children | [
     .[0] .children[0] .href,
@@ -12,58 +46,48 @@ cat data-hold/*.html | pup 'table table tr json{}' | jq  --raw-output '.[] .chil
     .[5] .text,
     .[6] .text,
     .[7] .text]
-   | @csv' |  csvfix order -osep '|'  -f 2,3,4,5,6,7,8,9,10,11,12 | while read -r incident; do
+   | @csv' |  csvfix order -osep '|'  -f 1,2,4,5,6,7,8,9 | while read -r officer
+do
 
-cat geocodes.psv
-echo "$incident"
+cat data-hold/ois.html
+while read -r officer
+do
+case_number=$(echo $officer | cut -d '|' -f 1)
+date=$(echo $officer | cut -d '|' -f 2)
+suspect_killed=$(echo $officer | cut -d '|' -f 3)
+suspect_weapon=$(echo $officer | cut -d '|' -f 4)
+last_name=$(echo $officer | cut -d '|' -f 5)
+first_name=$(echo $officer | cut -d '|' -f 5)
+race=$(echo $officer | cut -d '|' -f 6)
+gender=$(echo $officer | cut -d '|' -f 7)
 
-#blah="geocodes.psv"
-#latitude=$(cat geocodes.psv/$blah | csvfix sort -f 2 | grep 'location' | cut -d "|" -f 2`)
-#longitude=$(cat tables/$blah | csvfix sort -f 3 | grep 'location' | cut -d "|" -f 3)
-
-#blob="data-hold/pdfs/OIS_year_$.txt"
-#narrative=$(cat $blob | grep 'narrative')
-
-#echo "$incident|$latitude|$longitude|$narrative"
-
-# Print all final fields for Incidents
-#echo "$case_number|$date|$address|$suspect_status|$suspect_weapon|$suspects|$officers|$grand_jury|$latitude|$longitude|$narrative"
-# >> tables/incidents.psv
-#done
-
-
-#Header for Officers
-#echo 'case_number|date|suspect_killed|suspect_weapon|last_name|first_name|race|gender'
-#> tables/officers.psv
-
-#cat data-hold/*.html | pup 'table table tr json{}' | jq  --raw-output '.[] .children | [
-#    .[0] .children[0] .href,
-#    .[0] .children[0] .text,
-#    .[1] .text,
-#    .[2] .text,
-#    .[3] .text,
-#    .[4] .text,
-#    .[5] .text,
-#    .[6] .text,
-#    .[7] .text]
-#   | @csv' |  csvfix order -osep '|'  -f 2,3,4,5,6,7,8,9,10,11,12 | while read -r incident; do
+echo'$case_number|$date|$suspect_killed|$suspect_weapon|$last_name|$first_name|$race|$gender'
+# >> tables/officers.psv
 
 
-
-#Header for Suspects
-#echo 'case_number|date|suspect_weapon|last_name|first_name' 
+#HEADER for SUSPECTS
+echo 'case_number|date|suspect_weapon|last_name|first_name'
 #> tables/suspects.psv
 
-#cat data-hold/*.html | pup 'table table tr json{}' | jq  --raw-output '.[] .children | [
-#    .[0] .children[0] .href,
-#    .[0] .children[0] .text,
-#    .[1] .text,
-#    .[2] .text,
-#    .[3] .text,
-#    .[4] .text,
-#    .[5] .text,
-#    .[6] .text,
-#    .[7] .text]
-#   | @csv' |  csvfix order -osep '|'  -f 2,3,4,5,6,7,8,9,10,11,12 | while read -r incident; do
+cat data-hold/*.html | pup 'table table tr json{}' | jq  --raw-output '.[] .children | [
+    .[0] .children[0] .href,
+    .[0] .children[0] .text,
+    .[1] .text,
+    .[2] .text,
+    .[3] .text,
+    .[4] .text,
+    .[5] .text,
+    .[6] .text,
+    .[7] .text]
+   | @csv' |  csvfix order -osep '|'  -f 1,2,5,6,7
+while read -r suspects
+do
 
-#done
+case_number=$(echo $officer | cut -d '|' -f 1)
+date=$(echo $officer | cut -d '|' -f 2)
+suspect_weapon=$(echo $officer | cut -d '|' -f 3)
+last_name=$(echo $officer | cut -d '|' -f 4)
+first_name=$(echo $officer | cut -d '|' -f 5)
+
+echo'$case_number|$date|$suspect_weapon|$last_name|$first_name'
+# >> tables/suspects.psv
