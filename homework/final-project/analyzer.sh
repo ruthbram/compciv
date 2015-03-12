3ed367f887aef9ecb2ea2ebec35a384e9cdb5b6a
Ruth Clausen Bram <rcbram@corn12.stanford.edu>
2015-03-11 20:22:57 -0700

    almost done with analyzer

--- homework/final-project/analyzer.sh
+++ homework/final-project/analyzer.sh
@@ -1,14 +1,30 @@
 #Reference these file names to parse data: 0910cohortdata.txt  1011cohortdata.txt  1112cohortdata.txt  1213cohortdata.txt  locations.txt
+#In each file, I'm looking for CDS, Name, NumGraduates, and NumDropouts.
+#Then I will attach the addresses, located in locations.txt
 
-#Analyze 2009-2010 data
-cat data-hold/0910cohortdata.txt | cut -f 1,2,8,10 | grep -v '*'
 
-#Analyze 2010-2011 data
-cat data-hold/1011cohortdata.txt | cut -f 1,2,8,10 | grep -v '*'
+#Make new directory to hold csv files
 
-#Analyze 2011-2012 data
-cat data-hold/1112cohortdata.txt | cut -f 1,2,8,10 | grep -v '*'
+#Loop through the four txt files to output
+#CDS, Name, NumGraduates, NumDropouts, and Addresses
+for thing in 0910 1011 1112 1213; do
+    fname="csv-files/${thing}.csv"
+    echo 'Cohord data for $thing' > $fname/finaldata.csv
+    echo 'CDS|Name|NumGraduates|NumDropouts' >> $fname/finaldata.csv
+    echo $thing
+    cat data-hold/${thing}cohortdata.txt | cut -f 1,2,8,10 | grep -v '*' >> $fname/finaldata.csv
 
-#Analyze 2012-2013 data
-cat data-hold/1213cohortdata.txt | cut -f 1,2,8,10 | grep -v '*'
+#Continue lool to attach the addresses
+    cat data-hold/${thing}cohortdata.txt | grep 'FEM' | grep '9' |
+    csvfix read_dsv -s '\t' -osep '|' -smq |
+    while read -r line; do 
+    # thing|thing2|thing3
+    id=$(echo "$line" | cut -d '|' -f 1)
+    addr=$(cat data-hold/locations.txt | grep $id | csvfix read_dsv -smq -s '\t' -osep '|' | cut -d '|' -f 14,15)
+    stuff=$(echo "$line" | cut -d '|' -f 2,8,10)
+    echo "$id|$addr|$stuff"
+    done | grep -v '*' 
+    >> finaldata.csv
+done
+
 
--- homework/final-project/scraper.sh
+++ homework/final-project/scraper.sh
@@ -3,14 +3,10 @@ mkdir -p data-hold
 cd data-hold
 
 #Scrape Cohort Data 
-curl http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=All&cYear=2012-13&cCat=Cohort&Cpage=filescohort > 1213cohortdata.txt
-sleep1
-curl http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=All&cYear=2011-12&cCat=Cohort&Cpage=filescohort > 1112cohortdata.txt
-sleep1
-curl http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=All&cYear=2010-11&cCat=Cohort&Cpage=filescohort > 1011cohortdata.txt
-sleep 1
-curl http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=All&cYear=2009-10&cCat=Cohort&cPage=filescohort > 0910cohortdata.txt
-sleep 1
+curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=All&cYear=2012-13&cCat=Cohort&Cpage=filescohort" > 1213cohortdata.txt
+curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=All&cYear=2011-12&cCat=Cohort&Cpage=filescohort" > 1112cohortdata.txt
+curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=All&cYear=2010-11&cCat=Cohort&Cpage=filescohort" > 1011cohortdata.txt
+curl "http://dq.cde.ca.gov/dataquest/dlfile/dlfile.aspx?cLevel=All&cYear=2009-10&cCat=Cohort&cPage=filescohort" > 0910cohortdata.txt
 
 #Scrape locations of each school
 curl ftp://ftp.cde.ca.gov/demo/schlname/pubschls.txt > locations.txt
